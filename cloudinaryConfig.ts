10c6dcbdf40344552c5ba531f2ae46d0cc66be03
KristijanPavlic <pavlictumpakristijan@gmail.com>
2024-10-14 22:20:30 +0200

    moved cloudinary config variables to .env file

--- cloudinaryConfig.ts
+++ cloudinaryConfig.ts
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 // It's recommended to use environment variables for sensitive information
 cloudinary.config({
-  cloud_name: "dn0ngtrru",
-  api_key: "454919534882527",
-  api_secret: "V_X1tuwu-bibyltCHU43c_-nbzw",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
